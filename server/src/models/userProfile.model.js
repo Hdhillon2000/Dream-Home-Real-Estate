c8b9cea340dcc52cf5e52c59d1e7d7a0457f5dd8
goodsie <mgoodie@outlook.com>
2025-11-22 17:19:18 -0500

    cleanup: remove redundant fields selector. @See { select } of Model class options

--- server/src/models/userProfile.model.js
+++ server/src/models/userProfile.model.js
@@ -2,8 +2,7 @@
 import createModel from '../mysql/model.js';
 
 
-const UserProfileModel = await createModel('UserProfiles', {
-  primaryKey: 'profile_id',
-  fields: ['profile_id', 'user_id', 'first_name', 'last_name']
-});
+const UserProfileModel = await createModel('UserProfiles', { primaryKey: 'profile_id' });
+
+
 export default UserProfileModel;
